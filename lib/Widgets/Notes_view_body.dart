494b3d97170af261e1591888af9abcb45effc722
Abdelrahman2335 <125411627+Abdelrahman2335@users.noreply.github.com>
2023-08-16 13:40:43 +0300

    adding note item

--- lib/Widgets/Notes_view_body.dart
+++ lib/Widgets/Notes_view_body.dart
@@ -15,6 +15,55 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+            borderRadius: BorderRadius.circular(16)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title:
+            Text("Flutter Tips",
+              style: TextStyle(
+                  color: Colors.black,
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: (){},
+              icon:
+              Icon(
+                Icons.delete,
+                color: Colors.black,),
+            ),
+            subtitle:
+            Text("build your career with Tharwat Samy",
+              style: TextStyle(color: Colors.black,
+            ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Text(
+              "May 21 , 2022",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
         ],
       ),
     );
